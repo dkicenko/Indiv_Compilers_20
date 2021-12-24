0cab0fdcc61a75eb26c214a70016f8bbb097899e
Kicenko DM <darya.kitsenko@yandex.ru>
2021-12-24 18:14:03 +0300

    final fix

--- individual_task/Visitors/CorrectLengthVisitor.cs
+++ individual_task/Visitors/CorrectLengthVisitor.cs
@@ -20,8 +20,7 @@ namespace SimpleLang.Visitors
             int reallen;
             if (a.Id is SliceNode)
             {
-                //if (a.Expr is BinaryNode && !(a.Expr as BinaryNode).isarray)
-                //    throw new Exception("Неверное присваивание");
+                
                 if ((a.Id as SliceNode).Stop == int.MaxValue)
                     (a.Id as SliceNode).Stop = arrays[(a.Id as SliceNode).Name];
                 reallen = ((a.Id as SliceNode).Stop - (a.Id as SliceNode).Start)/ (a.Id as SliceNode).Step;
@@ -32,14 +31,12 @@ namespace SimpleLang.Visitors
             }
             if (a.Id is IdNode)
             {
-                //if (a.Expr is BinaryNode && !(a.Expr as BinaryNode).isarray)
-                //    throw new Exception("Неверное присваивание");
+               
                 reallen = arrays[(a.Id as IdNode).Name];
                 if (reallen < correct)
                     throw new Exception("Несовпадение размеров массивов");
             }
-            //if (a.Expr is BinaryNode && !(a.Expr as BinaryNode).isarray)
-            //    throw new Exception("Неверное присваивание");
+           
         }
         public override void VisitVarDefNode(VarDefNode w)
         {            
@@ -47,17 +44,16 @@ namespace SimpleLang.Visitors
             {
                 if (v is ArrayNode)
                 {
-                    //v.Visit(this);
+                   
                     arrays[(v as ArrayNode).Name] = (v as ArrayNode).Length;
                 }
                 if (v is IdNode)
                 {
-                   // v.Visit(this);
+                   
                     ids.Add((v as IdNode).Name);
                 }
             }
-            //foreach (var v in arrays)
-             //   Console.WriteLine(v);
+           
         }
         public override void VisitArrayNode(ArrayNode w)
         {
@@ -81,36 +77,7 @@ namespace SimpleLang.Visitors
 
         public override void VisitBinOpNode(BinaryNode binop)
         {
-           /* bool r = false, l = false;
-            if (binop.Right is IntNumNode)
-                r = false;
-            if (binop.Right is IdNode)
-            {
-                if (arrays.ContainsKey((binop.Right as IdNode).Name))
-                    r = true;
-                else
-                    r = false;                
-            }
-            if (binop.Right is ArrayNode || binop.Right is SliceNode)
-                r = true;
-            if (binop.Right is BinaryNode)
-                r = (binop.Right as BinaryNode).isarray;
-            if (binop.Left is IntNumNode)
-                l = false;
-            if (binop.Left is IdNode)
-            {
-                if (arrays.ContainsKey((binop.Left as IdNode).Name))
-                    l = true;
-                else
-                    l = false;
-            }
-            if (binop.Left is ArrayNode || binop.Left is SliceNode)
-                l = true;
-            if (binop.Left is BinaryNode)
-                l = (binop.Left as BinaryNode).isarray;
-            if (l != r)
-                throw new Exception("В правой части встречен массив одновремнно с не массивом");
-            binop.isarray = l && r;*/
+           
             binop.Left.Visit(this);
             binop.Right.Visit(this);
         }
